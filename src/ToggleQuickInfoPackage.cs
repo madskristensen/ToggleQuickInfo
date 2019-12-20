71d172da9eb31e3a323ca6243ad4b599114df1f8
Mads Kristensen <madskristensen@users.noreply.github.com>
2019-12-20 13:48:27 -0800

    Clean up [skip ci]

--- src/ToggleQuickInfoPackage.cs
+++ src/ToggleQuickInfoPackage.cs
@@ -1,23 +1,20 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.Shell;
+using System;
 using System.Runtime.InteropServices;
 using System.Threading;
-using EnvDTE;
-using Microsoft;
-using Microsoft.VisualStudio.Shell;
 using Task = System.Threading.Tasks.Task;
 
 namespace ToggleQuickInfo
 {
 	[PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
 	[InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
-	//[ProvideOptionPage(typeof(DialogPageProvider.General), "Environment", "Tooltips", 0, 0, true, new[] { "Toggle Tooltips" }, SupportsProfiles = true, ProvidesLocalizedCategoryName = false)]
 	[Guid(PackageGuids.guidToggleQuickInfoPackageString)]
 	[ProvideMenuResource("Menus.ctmenu", 1)]
 	public sealed class ToggleQuickInfoPackage : AsyncPackage
 	{
 		protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
 		{
-		    await ToggleCommand.InitializeAsync(this);
+			await ToggleCommand.InitializeAsync(this);
 		}
 	}
 }
