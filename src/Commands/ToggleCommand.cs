700af3cac440dc538a2082b85c86b4dee19ef833
Mads Kristensen <madskristensen@users.noreply.github.com>
2022-02-05 08:43:42 -0800

    Added VS 2022 support

--- src/Commands/ToggleCommand.cs
+++ src/Commands/ToggleCommand.cs
@@ -1,9 +1,9 @@
-﻿using EnvDTE;
-using Microsoft;
+﻿using Microsoft;
 using Microsoft.VisualStudio.Shell;
-
+using Microsoft.VisualStudio.Shell.Interop;
 using System;
 using System.ComponentModel.Design;
+using Community.VisualStudio.Toolkit;
 
 using Task = System.Threading.Tasks.Task;
 
@@ -17,22 +17,23 @@ namespace ToggleQuickInfo
 			await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
 
 			var commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
-			var dte = await package.GetServiceAsync(typeof(DTE)) as DTE;
 			Assumes.Present(commandService);
 
 			var menuCommandID = new CommandID(PackageGuids.guidToggleQuickInfoPackageCmdSet, PackageIds.ToggleCommandId);
-			var menuItem = new MenuCommand((s, e) => Execute(dte), menuCommandID);
+			var menuItem = new MenuCommand((s, e) => Execute(), menuCommandID);
 			commandService.AddCommand(menuItem);
 		}
 
-		private static void Execute(DTE dte)
+		private static void Execute()
 		{
 			ThreadHelper.ThrowIfNotOnUIThread();
 			
 			GeneralOptions.Instance.ShowTooltips = !GeneralOptions.Instance.ShowTooltips;
 			GeneralOptions.Instance.Save();
 
-			dte.StatusBar.Text = GeneralOptions.Instance.ShowTooltips ? "Tooltips enabled" : "Tooltips disabled";
+			string text = GeneralOptions.Instance.ShowTooltips ? "Tooltips enabled" : "Tooltips disabled";
+
+            VS.StatusBar.ShowMessageAsync(text).FireAndForget();
 		}
 	}
 }
